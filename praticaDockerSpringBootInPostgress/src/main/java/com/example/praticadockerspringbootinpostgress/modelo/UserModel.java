43a7aa632162a33f68e1077daed360233dae7baa
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-11 20:49:52 -0500

    Update UserModel.java

--- praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/modelo/UserModel.java
+++ praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/modelo/UserModel.java
@@ -1,4 +1,16 @@
 package com.example.praticadockerspringbootinpostgress.modelo;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tbl_users")
+@Data
 public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+
 }
