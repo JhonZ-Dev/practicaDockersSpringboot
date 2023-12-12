659d2968ae0b4b9d27bbf55b92a098ec4d643659
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-11 20:52:32 -0500

    Update UserController.java

--- praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/controlador/UserController.java
+++ praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/controlador/UserController.java
@@ -1,4 +1,9 @@
 package com.example.praticadockerspringbootinpostgress.controlador;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 }
