ab611608439eeecfbeb582b7260d3e5030ab8a31
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-11 20:58:25 -0500

    Update UserController.java

--- praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/controlador/UserController.java
+++ praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/controlador/UserController.java
@@ -1,9 +1,31 @@
 package com.example.praticadockerspringbootinpostgress.controlador;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.praticadockerspringbootinpostgress.modelo.UserModel;
+import com.example.praticadockerspringbootinpostgress.repositorio.UserRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    public UserRepositorio userRepositorio;
+
+    @GetMapping
+    public List<UserModel> obtenerUsuarios(){
+        return  userRepositorio.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserModel getUserById(@PathVariable Long id){
+        return userRepositorio.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public UserModel crearUsuario(@RequestBody UserModel userModel){
+        return userRepositorio.save(userModel);
+    }
 }
