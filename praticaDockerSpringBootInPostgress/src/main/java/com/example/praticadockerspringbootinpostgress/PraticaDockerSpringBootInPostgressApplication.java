4e239dea2fdf9d33779e5ef9c0bcb8cefbc346fe
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-12-11 20:32:43 -0500

    Create Project

---
+++ praticaDockerSpringBootInPostgress/src/main/java/com/example/praticadockerspringbootinpostgress/PraticaDockerSpringBootInPostgressApplication.java
@@ -0,0 +1,13 @@
+package com.example.praticadockerspringbootinpostgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaDockerSpringBootInPostgressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraticaDockerSpringBootInPostgressApplication.class, args);
+    }
+
+}
---
+++ praticaDockerSpringBootInPostgress/src/test/java/com/example/praticadockerspringbootinpostgress/PraticaDockerSpringBootInPostgressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praticadockerspringbootinpostgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaDockerSpringBootInPostgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
