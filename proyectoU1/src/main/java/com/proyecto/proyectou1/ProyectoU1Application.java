2a35e636a52b82df6f49db2caa30d542019f58ed
Jhon Zambrano <79710173+JhonZ-Dev@users.noreply.github.com>
2023-12-13 23:06:53 -0500

    Update ProyectoU1Application.java

--- proyectoU1/src/main/java/com/proyecto/proyectou1/ProyectoU1Application.java
+++ proyectoU1/src/main/java/com/proyecto/proyectou1/ProyectoU1Application.java
@@ -1,4 +1,7 @@
+package com.proyecto.proyectou1;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU1Application {
